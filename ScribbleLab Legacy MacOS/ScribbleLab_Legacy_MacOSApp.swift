093e84c25b87b9b81df12465ae1c593538965e0a
Kilian Balaguer <122736906+kilian-balaguer@users.noreply.github.com>
2024-03-01 16:26:44 +0100

    Changed The Files From obj-c to Swift + Storyboard  and also to swiftUI

---
+++ ScribbleLab Legacy MacOS/ScribbleLab_Legacy_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleLab_Legacy_MacOSApp.swift
+//  ScribbleLab Legacy MacOS
+//
+//  Created by Kilian Balaguer on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleLab_Legacy_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
